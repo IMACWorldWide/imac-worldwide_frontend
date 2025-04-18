5dc0fed27edebc5c7ffeaeda715499cb5e344473
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-04-18 22:43:31 +0200

    feat: Add Footer component with styles

---
+++ src/containers/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+/* Footer.tsx */
+
+// Importing CSS styles
+import './Footer.scss';
+
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>&copy; 2025 Colin OLIVIER. All rights reserved.</p>
+        </footer>
+    );
+}
