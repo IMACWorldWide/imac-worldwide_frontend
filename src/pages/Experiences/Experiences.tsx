9ca7d288c02c5b246ab7d43f61ec9f476603e57c
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-04-18 22:37:59 +0200

    feat: Enhance Experiences component with filter state management and session storage

--- src/pages/Experiences/Experiences.tsx
+++ src/pages/Experiences/Experiences.tsx
@@ -4,7 +4,7 @@
 import './Experiences.scss';
 
 // Importing React and necessary hooks
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Importing components
 import ExperienceCardContainer from "../../containers/ExperienceCardContainer/ExperienceCardContainer";
@@ -32,11 +32,39 @@ export default function Experiences() {
     const tags = tagsHook.tags; // Extracting the tags data from the hook
 
     const [filters, setFilters] = useState<FiltersType>({
-        countries: Object.fromEntries(countries.map((country: CountryType) => [country.code, true])),
-        tags: Object.fromEntries(tags.map((tag: TagType) => [tag.name, true])),
+        countries: {},
+        tags: {},
         languages: {},
         date: { startDate: new Date(), endDate: new Date() }
     });
+
+    useEffect(() => {
+        if (sessionStorage.getItem('experiencesFilters')) {
+            const storedFilters = sessionStorage.getItem('experiencesFilters'); // Retrieving filters from local storage
+            if (storedFilters) {
+                const parsedFilters = JSON.parse(storedFilters); // Parsing the filters from local storage
+                setFilters({ ...parsedFilters, date: { startDate: new Date(), endDate: new Date() } }); // Parsing and setting the filters state
+            }
+            return; // Exiting if filters are found in local storage
+        }
+        if (experiencesHook.loading === false && countriesHook.loading === false && tagsHook.loading === false) {
+            const initialFilters: FiltersType = {
+                countries: Object.fromEntries(countries.map((country: CountryType) => [country.code, false])),
+                tags: Object.fromEntries(tags.map((tag: TagType) => [tag.name, false])),
+                languages: {},
+                date: { startDate: new Date(), endDate: new Date() }
+            };
+            setFilters(initialFilters); // Setting the initial filters state
+        }
+    }, [experiencesHook.loading, countriesHook.loading, tagsHook.loading]); // Empty useEffect to mimic componentDidMount
+
+    useEffect(() => {
+        // Remove date from filters
+        const { date, ...rest } = filters; // Destructuring to remove date from filters
+        const newFilters = { ...rest }; // Creating a new filters object with default date values
+        sessionStorage.setItem('experiencesFilters', JSON.stringify(newFilters)); // Storing the filters in session storage
+    }
+        , [filters]); // Updating local storage whenever filters change
 
     const { filteredExperiences } = useFilteredExperiences(experiences, filters); // Using the custom hook to filter experiences based on selected filters
 
@@ -47,6 +75,8 @@ export default function Experiences() {
     };
 
     if (experiencesHook.loading || countriesHook.loading || tagsHook.loading) return <div>Loading...</div>; // Show loading state while fetching data
+
+    console.log("Filters: ", filters); // Logging the experiences data for debugging
 
     return (
         <main>
